e4f213cef187db3afe0048e78a12d1bc3177c5a0
Davidhzk2 <dherrera035@misena.edu.co>
2021-07-06 19:40:58 -0500

    Comments in PatronObservable and new file MisIdeasPatronObserver

--- PatronObserver.js
+++ PatronObserver.js
@@ -1,16 +1,18 @@
+// creacion de la clase
 class Observable {
     constructor(){
         this.observers =[];
     }
-
+    // Agregar notificacion al observers
     subscribe(notifyingClass){
         this.observers.push(notifyingClass);
     }
-
+    // Remover notificacion del observers
     unsubscribe(notifyingClass){
-        this.observers = this.observers.filter(observer => observer instanceof notifyingClass !== true)
+        this.observers = this.observers.filter(observer => observer instanceof notifyingClass !== true);
     }
 
+    // Recorrer el observers por elemento y se actualiza 
     notify(model){
         this.observers.forEach(element => {
             observer =>{
@@ -21,31 +23,33 @@ class Observable {
 
 }
 
+// Clase que heredad del observble, y lanzar las notioficaciones. 
 class NumberExample extends Observable{
     constructor(){
         super();
         this.value = 0;
     }
-
+    // Incrementador de la variable de uno a uno y notifica a los observadores
     increment(){
         this.value++;
         this.notify(this);
     }
     
 }
-
+// Mostrar el mensaje en español y la informacion actualizada y clase observadora 
 class NumberExampleSpanish{
     notify(model){
         console.log(`El nuevo numero es :${model.value}`);
     }
 }
-
+// Mostrar el mensaje en Ingles 
 class NumberExampleEnglish{
     notify(model){
         console.log(`The new number is :${model.value}`);
     }
 }
 
+// crear objeto de mi numberExample
 let numberExample = new NumberExample();
 
 numberExample.subscribe(new NumberExampleSpanish());
